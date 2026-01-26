ba6d8f62b50c61b6fe8490c3430c24160a28c0ea
Anmol Rajput <anmolrajput1103@gmail.com>
2026-01-26 18:56:39 +0530

    Firebase intergation

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxMXV4agjCwvVwy3tC9bGD6zToiUKjtnc",
+  authDomain: "linkforest-ca150.firebaseapp.com",
+  projectId: "linkforest-ca150",
+  storageBucket: "linkforest-ca150.firebasestorage.app",
+  messagingSenderId: "834188422747",
+  appId: "1:834188422747:web:d5dee5cdde2eb82cb4997e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
